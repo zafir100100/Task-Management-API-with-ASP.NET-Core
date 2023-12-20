9a337ff886d67894274258c9434d6a4986b7bb8a
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-12-20 20:36:12 +0600

    documenatation done for repository of user task

--- OrderProcessingSystemDotnet/OrderProcessingSystemDotnet/Repositories/UserTaskRepository.cs
+++ OrderProcessingSystemDotnet/OrderProcessingSystemDotnet/Repositories/UserTaskRepository.cs
@@ -6,22 +6,37 @@ using OrderProcessingSystemDotnet.Models.Tables;
 
 namespace OrderProcessingSystemDotnet.Repositories
 {
+    /// <summary>
+    /// Repository for managing UserTask operations.
+    /// </summary>
     public class UserTaskRepository : IUserTaskService
     {
         private ResponseDto _responseDto = new();
         private readonly TaskManagerDbContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the UserTaskRepository class.
+        /// </summary>
+        /// <param name="context">The TaskManagerDbContext used for database operations.</param>
         public UserTaskRepository(TaskManagerDbContext context)
         {
             _context = context;
         }
 
-        // Method to get tasks without transactions and async
+        /// <summary>
+        /// Retrieves a list of UserTasks asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A Task object representing the asynchronous operation, containing a ResponseDto with the list of tasks.
+        /// </returns>
         public async Task<ResponseDto> GetTasks()
         {
             try
             {
+                // Retrieve UserTasks from the database and order them by Id.
                 List<UserTask> tasks = await _context.UserTasks.OrderBy(i => i.Id).ToListAsync();
+
+                // Check if there are tasks available.
                 if (tasks.Count > 0)
                 {
                     _responseDto.Message = "Displaying the list of " + tasks.Count + " task(s).";
@@ -31,19 +46,27 @@ namespace OrderProcessingSystemDotnet.Repositories
                         RowCount = tasks.Count
                     };
                     _responseDto.StatusCode = StatusCodes.Status200OK;
+
+                    // Return the populated ResponseDto with task information.
                     return _responseDto;
                 }
                 else
                 {
+                    // No tasks found; provide a message indicating the need to create a task.
                     _responseDto.Message = "No task found. Please create one to view.";
                     _responseDto.StatusCode = StatusCodes.Status404NotFound;
+
+                    // Return the ResponseDto with a 404 status code.
                     return _responseDto;
                 }
             }
             catch (Exception ex)
             {
+                // An exception occurred while fetching the task list; provide an error message.
                 _responseDto.Message = "Unable to fetch task list. Something went wrong. Please try again later.";
                 _responseDto.StatusCode = StatusCodes.Status500InternalServerError;
+
+                // Populate Payload with exception details for troubleshooting.
                 _responseDto.Payload = new
                 {
                     ex.StackTrace,
@@ -52,32 +75,41 @@ namespace OrderProcessingSystemDotnet.Repositories
                     ex.Source,
                     ex.Data
                 };
+
+                // Return the ResponseDto with a 500 status code and exception details.
                 return _responseDto;
             }
         }
 
-        // Method to create a new task
+        /// <summary>
+        /// Creates a new UserTask asynchronously.
+        /// </summary>
+        /// <param name="newTask">The UserTask object to be created and added to the database.</param>
+        /// <returns>
+        /// A Task object representing the asynchronous operation, containing a ResponseDto with details of the created task.
+        /// </returns>
         public async Task<ResponseDto> CreateTask([FromBody] UserTask newTask)
         {
             try
             {
-                // Add the new task to the database and save changes
+                // Add the new task to the database and save changes.
                 _context.UserTasks.Add(newTask);
                 await _context.SaveChangesAsync();
 
-                // Set the response details for a successful creation
+                // Set the response details for a successful creation.
                 _responseDto.StatusCode = StatusCodes.Status201Created;
                 _responseDto.Message = "Task created successfully.";
 
+                // Return the ResponseDto indicating successful task creation.
                 return _responseDto;
             }
             catch (Exception ex)
             {
-                // Handle exceptions that may occur during the creation process
+                // Handle exceptions that may occur during the creation process.
                 _responseDto.StatusCode = StatusCodes.Status500InternalServerError;
                 _responseDto.Message = "Task creation failed. Something went wrong. Please try again later.";
 
-                // Additional information about the exception for debugging or logging
+                // Additional information about the exception for debugging or logging.
                 _responseDto.Payload = new
                 {
                     ex.StackTrace,
@@ -87,36 +119,53 @@ namespace OrderProcessingSystemDotnet.Repositories
                     ex.Data
                 };
 
+                // Return the ResponseDto with a 500 status code and exception details.
                 return _responseDto;
             }
         }
 
-        // Method to delete a task by ID
+        /// <summary>
+        /// Deletes a UserTask by its ID asynchronously.
+        /// </summary>
+        /// <param name="taskId">The ID of the UserTask to be deleted.</param>
+        /// <returns>
+        /// A Task object representing the asynchronous operation, containing a ResponseDto indicating the result of the deletion.
+        /// </returns>
         public async Task<ResponseDto> DeleteTask(int taskId)
         {
             try
             {
+                // Find the task to delete based on the provided taskId.
                 var taskToDelete = await _context.UserTasks.FindAsync(taskId);
 
+                // Check if the task exists.
                 if (taskToDelete == null)
                 {
                     _responseDto.Message = "No task found for the given criteria.";
                     _responseDto.StatusCode = StatusCodes.Status404NotFound;
+
+                    // Return the ResponseDto with a 404 status code.
                     return _responseDto;
                 }
 
+                // Remove the task from the database and save changes.
                 _context.UserTasks.Remove(taskToDelete);
                 await _context.SaveChangesAsync();
 
+                // Set the response details for a successful deletion.
                 _responseDto.Message = "Task deletion successful";
                 _responseDto.StatusCode = StatusCodes.Status200OK;
+
+                // Return the ResponseDto indicating successful task deletion.
                 return _responseDto;
             }
             catch (Exception ex)
             {
-                // Log or handle the exception as needed
+                // Log or handle the exception as needed.
                 _responseDto.StatusCode = StatusCodes.Status500InternalServerError;
                 _responseDto.Message = "Task deletion failed. Something went wrong. Please try again later.";
+
+                // Additional information about the exception for debugging or logging.
                 _responseDto.Payload = new
                 {
                     ex.StackTrace,
@@ -125,40 +174,59 @@ namespace OrderProcessingSystemDotnet.Repositories
                     ex.Source,
                     ex.Data
                 };
+
+                // Return the ResponseDto with a 500 status code and exception details.
                 return _responseDto;
             }
         }
 
-        // Method to update an existing task
+        /// <summary>
+        /// Updates a UserTask asynchronously.
+        /// </summary>
+        /// <param name="updatedTask">The UserTask object with updated information.</param>
+        /// <returns>
+        /// A Task object representing the asynchronous operation, containing a ResponseDto indicating the result of the update.
+        /// </returns>
         public async Task<ResponseDto> UpdateTask(UserTask updatedTask)
         {
             try
             {
+                // Find the existing task in the database based on the provided taskId.
                 var existingTask = await _context.UserTasks.FindAsync(updatedTask.Id);
+
+                // Check if the task exists.
                 if (existingTask == null)
                 {
                     _responseDto.Message = "No task found for the given criteria.";
                     _responseDto.StatusCode = StatusCodes.Status404NotFound;
+
+                    // Return the ResponseDto with a 404 status code.
                     return _responseDto;
                 }
 
+                // Update the existing task with the information from the updatedTask.
                 existingTask.Title = updatedTask.Title;
                 existingTask.Description = updatedTask.Description;
                 existingTask.DueDate = updatedTask.DueDate;
                 existingTask.Status = updatedTask.Status;
 
+                // Save changes to the database.
                 await _context.SaveChangesAsync();
 
+                // Set the response details for a successful update.
                 _responseDto.Message = "Task update successful";
                 _responseDto.StatusCode = StatusCodes.Status200OK;
 
+                // Return the ResponseDto indicating successful task update.
                 return _responseDto;
             }
             catch (Exception ex)
             {
-                // Log or handle the exception as needed
+                // Log or handle the exception as needed.
                 _responseDto.StatusCode = StatusCodes.Status500InternalServerError;
-                _responseDto.Message = "Task updated failed. Something went wrong. Please try again later.";
+                _responseDto.Message = "Task update failed. Something went wrong. Please try again later.";
+
+                // Additional information about the exception for debugging or logging.
                 _responseDto.Payload = new
                 {
                     ex.StackTrace,
@@ -167,24 +235,37 @@ namespace OrderProcessingSystemDotnet.Repositories
                     ex.Source,
                     ex.Data
                 };
+
+                // Return the ResponseDto with a 500 status code and exception details.
                 return _responseDto;
             }
         }
 
-        // Method to get a task by ID
+        /// <summary>
+        /// Retrieves a UserTask by its ID asynchronously.
+        /// </summary>
+        /// <param name="taskId">The ID of the UserTask to be retrieved.</param>
+        /// <returns>
+        /// A Task object representing the asynchronous operation, containing a ResponseDto with details of the retrieved task.
+        /// </returns>
         public async Task<ResponseDto> GetTaskById(int taskId)
         {
             try
             {
+                // Find the task in the database based on the provided taskId.
                 var task = await _context.UserTasks.FindAsync(taskId);
 
+                // Check if the task exists.
                 if (task == null)
                 {
                     _responseDto.Message = "No task found for given criteria.";
                     _responseDto.StatusCode = StatusCodes.Status404NotFound;
+
+                    // Return the ResponseDto with a 404 status code.
                     return _responseDto;
                 }
 
+                // Set the response details for a successful task retrieval.
                 _responseDto.Message = "Fetching task successful";
                 _responseDto.StatusCode = StatusCodes.Status200OK;
                 _responseDto.Payload = new
@@ -192,12 +273,16 @@ namespace OrderProcessingSystemDotnet.Repositories
                     Output = task
                 };
 
+                // Return the ResponseDto indicating successful task retrieval.
                 return _responseDto;
             }
             catch (Exception ex)
             {
+                // Log or handle the exception as needed.
                 _responseDto.StatusCode = StatusCodes.Status500InternalServerError;
                 _responseDto.Message = "Task retrieval failed. Something went wrong. Please try again later.";
+
+                // Additional information about the exception for debugging or logging.
                 _responseDto.Payload = new
                 {
                     ex.StackTrace,
@@ -206,6 +291,8 @@ namespace OrderProcessingSystemDotnet.Repositories
                     ex.Source,
                     ex.Data
                 };
+
+                // Return the ResponseDto with a 500 status code and exception details.
                 return _responseDto;
             }
         }
