e91ca3adb75c60648a423c0aa74981af980ee416
Zafir <55599023+zafir100100@users.noreply.github.com>
2024-09-24 19:44:03 +0600

    cors added

--- OrderProcessingSystemDotnet/OrderProcessingSystemDotnet/Program.cs
+++ OrderProcessingSystemDotnet/OrderProcessingSystemDotnet/Program.cs
@@ -9,6 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+// Configure CORS to allow all origins
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin() // Allow any origin
+               .AllowAnyMethod() // Allow any HTTP method (GET, POST, etc.)
+               .AllowAnyHeader(); // Allow any header
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
